1f02bc14da371f0ab5b0f193440d288f8a9a48e5
amirhpooladi <104260321+amirhpooladi@users.noreply.github.com>
2023-01-07 01:31:43 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 function clock() {
     setTimeout(() => {
         date = new Date;
@@ -16,15 +18,22 @@ clock();
 
 
 async function getData(lat, lon) {
+    
+    
     var result = await fetch(`https://developer.nrel.gov/api/pvwatts/v6.json?api_key=TCWMiZU5xjSoULqFkixsLi6yEG2GEaIWbipf0sq3&lat=${lat}&lon=${lon}&system_capacity=4&azimuth=180&tilt=0&array_type=0&module_type=0&losses=0&dataset=intl`)
     var jsonData = await result.json();
     var peakSunHours = jsonData.outputs.solrad_annual;
     console.log(peakSunHours);
-    peakSunHours = Math.round(peakSunHours);
+    var peakSunHours = Math.round(peakSunHours);
+    
 
-    ;
+    
 
-    if (peakSunHours == NaN) {
+
+    if (peakSunHours!=0&& peakSunHours!=1 && peakSunHours!=2&& peakSunHours!=3&& peakSunHours!=4&& peakSunHours!=5&& peakSunHours!=6&&peakSunHours!=7&& peakSunHours!=8&& peakSunHours!=9&& peakSunHours!=10&& peakSunHours!=11&& peakSunHours!=12 ) {
+        if(document.querySelector(".card")){
+            document.querySelector(".card").parentNode.removeChild(document.querySelector(".card"));
+        }
         document.getElementById("error").textContent = ":( خطا : مختصات این ناحیه قابل پوشش نیست";
     }
     else {
@@ -32,27 +41,33 @@ async function getData(lat, lon) {
         electric = 250 * peakSunHours * 75 / 100 / 1000;
         panel = electric * 1000 / 270;
         console.log(panel);
-        panel = Math.round(panel);
+        panel = Math.round(panel)+1;
         // console.log(peakSunHours)
         console.log(electric + " KW/H");
 
-        var addCard=`<div class="card" style="width: 18rem;">
+        if(document.querySelector(".card")){
+            document.querySelector(".card").parentNode.removeChild(document.querySelector(".card"));
+        }
+
+        var addCard = `<div class="card" style="width: 18rem;">
         <ul class="list-group list-group-flush">
             <li id="peak_li" class="list-group-item"></li>
             <li id="elec_li" class="list-group-item"></li>
             <li id="panel_li" class="list-group-item"></li>
         </ul>
     </div>`;
-        document.getElementById("lon").insertAdjacentHTML("afterend",
-        
+        document.getElementById("btn1").insertAdjacentHTML("afterend",
             addCard)
+            
+
+            
 
         document.getElementById("peak_li").textContent = ` تابش خورشید در روز = ${peakSunHours} `
         document.getElementById("elec_li").textContent = `KW/H  برق تولید شده= ${electric}`
         document.getElementById("panel_li").textContent = `تعداد پنل مورد نیاز= ${panel}`
-        
 
-        
+       
+
     }
 
 }
@@ -62,16 +77,19 @@ async function getData(lat, lon) {
 
 
 function click() {
+   
+   
 
     document.querySelector("#btn1").addEventListener("click", () => {
         var lat = document.getElementById("lat").value;
         var lon = document.getElementById("lon").value;
         getData(lat, lon)
+        
 
         document.getElementById("lat").value = "";
         document.getElementById("lon").value = "";
         document.getElementById("error").textContent = "";
-
+        
     })
 
 }
